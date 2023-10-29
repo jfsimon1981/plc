64c7da3e3150648d21519b4087a93e47243c41d9
Jean-Francois Simon <jfsimon@startmail.com>
2023-10-29 19:54:01 +0000

    Template

--- apps/template/main.cpp
+++ apps/template/main.cpp
@@ -29,55 +29,38 @@
  * SUCH DAMAGE.
  */
 
-namespace Plc {
+#include <plc++.h>
+#include "user_application.h"
 
-// ************* Motor Class *************
+// Main entry point
 
-class Motor {
-  private:
-    // Monitoring and protections
-    bool b_temperature_monitoring {0};
-    int i_winding_temperature_alarm {105};
-    int i_winding_temperature_trip {125};
-    int i_bearing_temperature_alarm {85};
-    int i_bearing_temperature_trip {90};
+main() {
+  /*
+   * Instanciate a Virtual PLC:
+   * 
+   * Virtual PLC are software core(s) to run applications and drive hardware.
+   * It is possible to instanciate and run multiple virtual PLCs on single
+   * hardware configuration.
+   * Virtual PLCs cores run independently from each other.
+   */
 
-    // Control
-    bool b_running {0};
-    bool b_tripped {0};
-    bool b_alarm {0};
-    bool b_trip {0};
-    bool b_brg_alarm {0};
-    bool b_brg_trip {0};
-    bool b_wdg_alarm {0};
-    bool b_wdg_trip {0};
+  Plc plc1;        // PLC core instance which run tasks and applications.
+  plc1.load(app1); // Load user application defined in user_application.cpp/.h
+  plc1.run();      // Set PLC in run. Possible commands are run() and stop().
 
-  public:
-    // Control
-    void start();
-    void stop();
-    void trip();
-    void set_speed(int);
-    void reset_trip();
+  /*
+   * Hypervisor is not required. Is useful to monitor CPU load and application
+   * status.
+   */
 
-    // Readback
-    bool is_healthy();
-    bool is_alarm();
-    bool is_trip();
-    bool is_brg_alarm();
-    bool is_brg_trip();
-    bool is_wdg_alarm();
-    bool is_wdg_trip();
-
-    // Protections
-    void set_winding_alarm_value(int);
-    void set_winding_trip_value(int);
-    void set_bearing_alarm_value(int);
-    void set_bearing_trip_value(int);
-    void enable_winding_alarm();
-    void enable_winding_trip();
-    void disable_winding_alarm();
-    void disable_winding_trip();
-};
-
+  Hypervisor hypervisor;
+  hypervisor.register(plc1);
+  while(1);
 }
+
+
+
+
+
+
+//
--- apps/template/user_application.cpp
+++ apps/template/user_application.cpp
@@ -29,55 +29,9 @@
  * SUCH DAMAGE.
  */
 
-namespace Plc {
+/*
+ * Implement applications here.
+ * List all available applications in user_application.h
+ */
 
-// ************* Motor Class *************
-
-class Motor {
-  private:
-    // Monitoring and protections
-    bool b_temperature_monitoring {0};
-    int i_winding_temperature_alarm {105};
-    int i_winding_temperature_trip {125};
-    int i_bearing_temperature_alarm {85};
-    int i_bearing_temperature_trip {90};
-
-    // Control
-    bool b_running {0};
-    bool b_tripped {0};
-    bool b_alarm {0};
-    bool b_trip {0};
-    bool b_brg_alarm {0};
-    bool b_brg_trip {0};
-    bool b_wdg_alarm {0};
-    bool b_wdg_trip {0};
-
-  public:
-    // Control
-    void start();
-    void stop();
-    void trip();
-    void set_speed(int);
-    void reset_trip();
-
-    // Readback
-    bool is_healthy();
-    bool is_alarm();
-    bool is_trip();
-    bool is_brg_alarm();
-    bool is_brg_trip();
-    bool is_wdg_alarm();
-    bool is_wdg_trip();
-
-    // Protections
-    void set_winding_alarm_value(int);
-    void set_winding_trip_value(int);
-    void set_bearing_alarm_value(int);
-    void set_bearing_trip_value(int);
-    void enable_winding_alarm();
-    void enable_winding_trip();
-    void disable_winding_alarm();
-    void disable_winding_trip();
-};
-
-}
+Plc::Application app1;
